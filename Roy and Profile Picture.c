a8b3893427559c64578a5a9508fe9ca6ea89d89c
Kanchan <88272489+Kanchan-Bartwal@users.noreply.github.com>
2022-04-21 10:56:35 +0530

    Create Roy and Profile Picture.c

---
+++ Roy and Profile Picture.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int l,n,w,h;
+	scanf("%d",&l);
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++)
+	{
+		scanf("%d%d",&w,&h);
+		if((w<l)||(h<l))
+		printf("UPLOAD ANOTHER\n");
+		else
+		{
+			if(w==h)
+			printf("ACCEPTED\n");
+			else
+			printf("CROP IT\n");
+		}
+	}
+	return 0;
+}
